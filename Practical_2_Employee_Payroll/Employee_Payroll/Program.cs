7dff078f86fd3aef1502d508590ddd2e5726234e
Krish-2771 <kotharikrish25@gmail.com>
2026-08-24 02:38:59 +0530

    Add Practical 02 and Practical 03

---
+++ Practical_2_Employee_Payroll/Employee_Payroll/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace P2_Employee_Payroll
+{
+    class Employee
+    {
+        public int emp_id;
+        public string emp_name;
+        public double emp_salary;
+        public Employee()
+        {
+            Console.WriteLine("--------------------------");
+            Console.WriteLine(" Employee Payroll System");
+            Console.WriteLine("--------------------------");
+        }
+        public void AccptDetails()
+        {
+            Console.Write("Enter Employee ID : ");
+            emp_id = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Employee Name : ");
+            emp_name = Console.ReadLine();
+            Console.Write("Enter Basic Salary :");
+            emp_salary = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Employee ID : "+ emp_id);
+            Console.WriteLine("Employee Name : "+ emp_name);
+            Console.WriteLine("Employee Basic Salary : "+ emp_salary);
+        }
+
+        public virtual void CalculateSalary()
+        {
+            Console.WriteLine("--------Salary Calculation-------- ");
+        }
+
+    }
+
+    class FullTimeEmployee : Employee
+    {
+        public override void CalculateSalary()
+        {
+            double HRA = emp_salary * .20;
+            double DA = emp_salary * .10;
+            double NetSalary = emp_salary + HRA + DA;
+            Console.WriteLine("Net Salary : "+ NetSalary);
+        }
+    }
+    class PartTimeEmployee : Employee
+    {
+        public override void CalculateSalary()
+        {
+            double NetSalary = emp_salary;
+            Console.WriteLine("Net Salary : "+ NetSalary);
+        }
+    }
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Employee Type :");
+            Console.WriteLine("1. Full Time Employee");
+            Console.WriteLine("2. Part Time Employee");
+            Console.Write("Enter your choice : ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            Employee employee = null;
+            if (choice == 1)
+            {
+                employee = new FullTimeEmployee();
+            }
+            else if (choice == 2)
+            {
+                employee = new PartTimeEmployee();
+            }
+            else
+                Console.WriteLine("Invalid Input.");
+
+            employee.AccptDetails();
+            employee.DisplayDetails();
+            employee.CalculateSalary();
+
+        }
+    }
+}
---
+++ Practical_3_Expense_Tracker/Expense_Tracker/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3_Expense_Tracker
+{
+    class Expense
+    {
+        public int ExpenseId;
+        public string Category;
+        public Double Amount;
+        public string paymentMethod;
+        public DateTime ExpenseDate;
+
+        public void AddExpenes()
+        {
+            Console.Write("Enter Expense ID: ");
+            ExpenseId = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Expense Category: ");
+            Category = Console.ReadLine();
+
+            Console.Write("Enter Expense Amount: ");
+            Amount = Convert.ToDouble(Console.ReadLine());
+
+            if (Amount <= 0)
+            {
+                throw new Exception("Amount should be greater than zero.");
+            }
+
+            Console.Write("Enter Payment Method (cash/upi/card): ");
+            paymentMethod = Console.ReadLine();
+
+            ExpenseDate = DateTime.Now;
+        }
+
+        public void DisplayExpense()
+        {
+            Console.WriteLine("==================================");
+            Console.WriteLine("Expense ID: " + ExpenseId);
+            Console.WriteLine("Expense Category: " + Category);
+            Console.WriteLine("Expense Amount: " + Amount);
+            Console.WriteLine("Payment Method: " + paymentMethod);
+            Console.WriteLine("Expense Date: " + ExpenseDate);
+            Console.WriteLine("==================================");
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Expense> expenses = new List<Expense>();
+
+            void menu()
+            {
+                Console.WriteLine("=============================");
+                Console.WriteLine("1. Add Expense");
+                Console.WriteLine("2. View All Expense");
+                Console.WriteLine("3. View Total Expense");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("=============================");
+            }
+
+            while (true)
+            {
+                try
+                {
+                    menu();
+                    Console.Write("Enter your choice: ");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+
+                    if (choice == 1)
+                    {
+                        Expense E1 = new Expense();
+                        E1.AddExpenes();
+                        expenses.Add(E1);
+                    }
+                    else if (choice == 2)
+                    {
+                        if (expenses.Count == 0)
+                        {
+                            Console.WriteLine("No expenses found.");
+                            continue;
+                        }
+
+                        foreach (Expense expense in expenses)
+                        {
+                            expense.DisplayExpense();
+                        }
+                    }
+                    else if (choice == 3)
+                    {
+                        double temp = 0;
+
+                        foreach (Expense expense in expenses)
+                        {
+                            temp += expense.Amount;
+                        }
+
+                        Console.Write("Total Expense : " + temp);
+                        Console.WriteLine();
+                    }
+                    else if (choice == 4)
+                    {
+                        Console.WriteLine("Exiting...");
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid choice");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Exception occur : {e.Message}");
+                }
+            }
+        }
+    }
+}
